3df2d865e549801138223a60da758317d9075402
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-10-25 20:08:46 +0200

    Improve Veda documentation

--- test/documentation_test.rb
+++ test/documentation_test.rb
@@ -2,19 +2,23 @@ require './test/test_helper'
 
 describe "Veda::Documentation" do
   before do
-    @page = Veda::Documentation.new("./test/test-repo")
+    @documentation = Veda::Documentation.new("./test/test-repo")
   end
 
   it "should extract metadata from a single file" do
-    meta = @page.fetch "file1"
+    meta = @documentation.fetch "file1"
   end
 
   it "should not crash when no yaml" do
-    meta = @page.fetch "empty"
+    meta = @documentation.fetch "empty"
   end
 
   it "should extract metadata from a collection of files" do
-    @page.collection
+    @documentation.collection
+  end
+
+  it "should extract an outline" do
+
   end
 
   it "should use a slug as path if present"
